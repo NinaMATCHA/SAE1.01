8e4598bc55afd498780a558121469a7802e31720
Nina MATIC CHARBIT <nina.MATIC-CHARBIT@etu.univ-amu.fr>
2026-01-01 18:53:21 +0100

    Update game functions for dynamic difficulty and PvP
    Refactor game logic to support variable difficulty and player modes.

--- tp10.h
+++ tp10.h
@@ -3,10 +3,6 @@
 
 using namespace std;
 
-const unsigned KMaxTimes (5);
-const unsigned KnbCandies(4);
-const unsigned KVide(0);
-
 typedef vector <unsigned> line; // un type représentant une ligne de la grille
 typedef vector <line> mat; // un type représentant la grille
 
@@ -15,9 +11,13 @@ struct maPosition {
     unsigned ord; // ligne
 }; // une position dans la grille
 
+// ----------------------------AFFICHAGE-----------------------------
+
 void clearScreen () {
     cout << "\033[H\033[2J";
 }
+
+const unsigned KVide(0);
 
 const unsigned KRouge   (31);
 const unsigned KVert    (32);
@@ -28,12 +28,12 @@ void couleur (const unsigned & coul) {
     cout << "\033[" << coul <<"m";
 }
 
-void initGrid (mat & grid, const size_t & matSize){
-    grid.resize(matSize);
-    for (size_t i(0); i<matSize; ++i){
-        grid[i].resize(matSize);
-        for (size_t j(0); j<matSize; ++j){
-            grid[i][j] = (rand()%KnbCandies)+1;
+void initGrid (mat & grid, const unsigned & nbCandy){ // le changement de difficulté, nbCandy n'est plus une val globale, et définit la taille
+    grid.resize(nbCandy);
+    for (size_t i(0); i<nbCandy; ++i){
+        grid[i].resize(nbCandy);
+        for (size_t j(0); j<nbCandy; ++j){
+            grid[i][j] = (rand()%nbCandy)+1;
         }
     }
 }
@@ -41,7 +41,7 @@ void initGrid (mat & grid, const size_t & matSize){
 void  displayGrid (const mat & grid){
     clearScreen();
     // indice d'une couleur dans la liste => couleur d'un nombre de cet indice
-    const vector<unsigned> color = {KRouge,KVert,KMAgenta,KBleu};
+    const vector<unsigned> color = {KRouge,KVert,KMAgenta,KBleu,};
 
     cout << "   ";
     for(size_t j=0; j<grid.size(); ++j){
@@ -74,37 +74,58 @@ bool isValid(const mat& grid, const maPosition& pos) {
 }
 
 // changer makeAMove de void en maPosition permet de garder la valeur de newPos
-maPosition makeAMove (mat & grid, const maPosition & pos, const char & direction){
+maPosition makeAMove (mat & grid, const maPosition & pos, const char & direction, bool isPlayer1){
     maPosition newPos = pos;
 
+    //selon le tour en pvp, change les touches valides
+    vector<char> keys;
+    if (isPlayer1)
+        keys = {'z','s','a','e'};
+    else
+        keys = {'o','l','k','m'};
+
+    bool isValidKey = false;
+    for(char key : keys){
+        if(key == direction)
+            isValidKey = true;
+    }
+
+    while(!isValidKey){
+        cout << "Mauvaises touches ! Elles sont indiquées ci-dessus !" << endl;
+        for(char key : keys){
+            if(key == direction)
+                isValidKey = true;
+        }
+    }
+
     // on évite les débordements en plus de changer pos
-    if (direction == 'z' && pos.ord > 0) --newPos.ord;
-    else if (direction == 's' && pos.ord < grid.size() - 1) ++newPos.ord;
-    else if (direction == 'a' && pos.abs > 0) --newPos.abs;
-    else if (direction == 'e' && pos.abs < grid.size() - 1) ++newPos.abs;
+    if ( direction == keys[0] && pos.ord > 0) --newPos.ord;
+    else if ( direction == keys[1]  && pos.ord < grid.size() - 1) ++newPos.ord;
+    else if ( direction == keys[2] && pos.abs > 0) --newPos.abs;
+    else if ( direction == keys[3] && pos.abs < grid.size() - 1) ++newPos.abs;
 
     // fonction swap pour échanger directement
     swap(grid[pos.ord][pos.abs], grid[newPos.ord][newPos.abs]);
     return newPos;
 }
 
-// ---------------------------------------------------------
+// ----------------------------ALIGNEMENTS-----------------------------
 
 bool atLeastThreeInAColumn (const mat & grid, maPosition & pos, unsigned & howMany){
     howMany = 1; //on compte le bonbon sur la position aussi
-    unsigned nbCandy = grid[pos.ord][pos.abs];
-    if (nbCandy == KVide) return false; // pas de points sur les cases vides
+    unsigned valCandy = grid[pos.ord][pos.abs];
+    if (valCandy == KVide) return false; // pas de points sur les cases vides
 
     // on verifie en haut
     unsigned li = pos.ord;
-    while (li > 0 && grid[li-1][pos.abs] == nbCandy){
+    while (li > 0 && grid[li-1][pos.abs] == valCandy){
         ++howMany;
         --li;
     }
 
     // en bas
     li = pos.ord;
-    while (li < grid.size() - 1 && grid[li+1][pos.abs] == nbCandy){
+    while (li < grid.size() - 1 && grid[li+1][pos.abs] == valCandy){
         ++howMany;
         ++li;
     }
@@ -114,19 +135,19 @@ bool atLeastThreeInAColumn (const mat & grid, maPosition & pos, unsigned & howMa
 
 bool atLeastThreeInARow (const mat & grid, maPosition & pos, unsigned & howMany){
     howMany = 1;
-    unsigned nbCandy = grid[pos.ord][pos.abs];
-    if (nbCandy == KVide) return false;
+    unsigned valCandy = grid[pos.ord][pos.abs];
+    if (valCandy == KVide) return false;
 
     // on verifie a gauche
     unsigned col = pos.abs;
-    while (col > 0 && grid[pos.ord][col-1] == nbCandy){
+    while (col > 0 && grid[pos.ord][col-1] == valCandy){
         ++howMany;
         --col;
     }
 
     // a droite
     col = pos.abs;
-    while (col < grid[0].size() - 1 && grid[pos.ord][col+1] == nbCandy){
+    while (col < grid[0].size() - 1 && grid[pos.ord][col+1] == valCandy){
         ++howMany;
         ++col;
     }
@@ -136,7 +157,7 @@ bool atLeastThreeInARow (const mat & grid, maPosition & pos, unsigned & howMany)
 
 // ---------------------------------------------------------
 
-void removalInColumn (mat & grid, maPosition & pos, const unsigned & howMany){
+void removalInColumn (mat & grid, maPosition & pos, const unsigned & howMany, const unsigned nbCandy, const bool infini){
 
     // on retrouve le début de la colonne de memes éléments
     unsigned currentCandy = grid[pos.ord][pos.abs];
@@ -154,12 +175,12 @@ void removalInColumn (mat & grid, maPosition & pos, const unsigned & howMany){
             grid[i][pos.abs] = grid[i-1][pos.abs];
         }
 
-        // la case en haut n'ont plus de valeurs
-        grid[0][pos.abs] = KVide;
+        // la case en haut n'ont plus de valeurs ou est remplacée selon si c'est le mode infini ou pas
+        grid[0][pos.abs] = (infini) ?  (rand()%nbCandy)+1 : KVide;
     }
 }
 
-void removalInRow (mat & grid, maPosition & pos, const unsigned & howMany){
+void removalInRow (mat & grid, maPosition & pos, const unsigned & howMany, const unsigned nbCandy, const bool infini){
     // on retrouve le début de la ligne de memes éléments
     unsigned currentCandy = grid[pos.ord][pos.abs];
     unsigned begin = pos.abs; // variable de position du debut des memes éléments
@@ -174,46 +195,62 @@ void removalInRow (mat & grid, maPosition & pos, const unsigned & howMany){
         for(unsigned i = pos.ord; i > 0; --i){
             grid[i][currentCol] = grid[i-1][currentCol];
         }
-        // La case du haut devient vide
-        grid[0][currentCol] = KVide;
+        // La case du haut devient vide ou est remplacée selon si c'est le mode infini ou pas
+        grid[0][currentCol] = (infini) ?  (rand()%nbCandy)+1 : KVide;
     }
 }
 
+// ---------------------------------------------------------
+
 // calcule le score et teste les alignements sur ses différentes positions
-unsigned scoreMatch(mat & grid, maPosition & pos, unsigned & howMany){
+unsigned scoreMatch(mat & grid, maPosition & pos, unsigned & howMany, const unsigned nbCandy, const bool infini){
     unsigned score = 0;
     unsigned valPos = grid[pos.ord][pos.abs]; // la valeur(chiffre) du bonbon
     if(valPos != KVide) {
         if(atLeastThreeInAColumn(grid, pos, howMany)){
-            removalInColumn(grid, pos, howMany);
+            removalInColumn(grid, pos, howMany, nbCandy, infini);
             score += valPos * howMany;
         }
         else if(atLeastThreeInARow(grid, pos, howMany)){
-            removalInRow(grid, pos, howMany);
+            removalInRow(grid, pos, howMany, nbCandy, infini);
             score += valPos * howMany;
         }
     }
     return score;
 }
 
-void tp10(){
-    // on initialise la grille du jeu ainsi que le nombre de coup maximal;
-    srand(time(0));
-    mat matrice;
-    initGrid(matrice,6);
+// ----------------------------JEU-----------------------------
 
-    cout << "Nombres de coup max : " << KMaxTimes;
-    unsigned coups = 0;
+void play( const unsigned maxTimes, const unsigned nbCandy, const bool pvp, const bool infini){ // on change la difficulté et les modes ici
+    // on initialise la grille du jeu ainsi que le nombre de coup maximal;
+    srand(time(0)); // permet de pouvoir rendre réellement aléatoire la matrice
+    mat matrice;
+    initGrid(matrice, nbCandy);
+
+    cout << "Nombres de coup max : " << maxTimes;
     char direction;
-    unsigned howMany;
-    unsigned score = 0;
+    unsigned howMany, score;
+    unsigned score1 = 0, score2 = 0, coups = 0, coups2 = 0; // score et coups pour différents joueurs
+    bool isPlayer1; // bool qui définit si c'est le joueur 1 qui joue ou non
+    unsigned turn = (pvp) ? rand()%2 : 0; // if else en une seule ligne, rand définit qui joue en premier en pvp, en dehors du pvp le j1 joue toujours
 
     // on entre dans une boucle tant qu’on n’a pas atteint le nombre maximal de coups :
-    while(coups < KMaxTimes){
-
+    while(coups < maxTimes && coups2 < maxTimes){
+        isPlayer1 = turn%2==0;
+        if(pvp){
+            if(isPlayer1){
+                coups++;
+                cout << "C'est le tour du joueur 1" << endl;
+            }
+            else{
+                coups2++;
+                cout << "C'est le tour du joueur 2" << endl;
+            }
+        }
+        score = (isPlayer1) ? score1 : score2; // le tour définit qui voit son score augmenter
         displayGrid(matrice); // affiche la grille;
         cout << "Votre score actuel : " << score << endl;
-        cout << "Encore " << KMaxTimes - coups << " coups !" << endl;
+        cout << "Encore " << maxTimes - coups << " coups !" << endl;
 
         // on saisit les coordonnées du bonbon a déplacer
         maPosition pos;
@@ -222,33 +259,67 @@ void tp10(){
         cout << "Ligne :";
         cin >> pos.ord;
 
-        if (!isValid(matrice, pos)) {
-            cout << "Veuillez suivre les positions indiquées"<< endl;
-            continue; // permet de recommencer du début la boucle
+        while(!isValid(matrice, pos)) {
+            cout << "La position est en dehors de la matrice ! Réessayez :"<< endl;
+            cout << "Colonne :";
+            cin >> pos.abs;
+            cout << "Ligne :";
+            cin >> pos.ord;
         }
-        coups++;
 
-        cout << "Ou voulez vous aller ? (a/z/e/s) : ";
+        if(isPlayer1)
+            cout << "Ou voulez vous aller ? (a/z/e/s) : ";
+        else
+            cout << "Ou voulez vous aller ? (o/k/l/m) : ";
+
         cin >> direction;
 
         // on récupère la nouvelle position en plus d'échanger les deux
-        maPosition newPos = makeAMove(matrice, pos, direction);
+        maPosition newPos = makeAMove(matrice, pos, direction,isPlayer1);
 
         while(newPos.abs == pos.abs && newPos.ord == pos.ord){
             cout << "Vous allez en dehors de la matrice ! Réessayez :";
             cin >> direction;
-            newPos = makeAMove(matrice, pos, direction);
+            newPos = makeAMove(matrice, pos, direction,isPlayer1);
         }
 
         //score et test sur l'ancienne position
-        score+=scoreMatch(matrice, pos, howMany);
+        score+=scoreMatch(matrice, pos, howMany, nbCandy, infini);
 
         //score et test sur la nouvelle position
         if(isValid(matrice, newPos)) {
-            score+=scoreMatch(matrice, newPos, howMany);
+            score+=scoreMatch(matrice, newPos, howMany, nbCandy, infini);
         }
+
+        if(isPlayer1)
+            score1 = score;
+        else
+            score2 = score;
+
+        //si c'est le mode pvp, on definit a qui sera le tour de jouer la prochaine fois
+        if(pvp)
+            ++turn;
+        else
+            coups2 = coups; // permet de pouvoir sortir du while a la fin du jeu
+
     }
 
     displayGrid(matrice);
-    cout << "FINI ! Votre score final :" << score << endl;
+    if(pvp){
+        cout << "FINI ! Vos scores finaux :" << endl;
+        cout << "Joueur 1 : " << score1 << endl;
+        cout << "Joueur 2 : " << score2 << endl;
+        if(score1>score2){
+            cout << "C'est le Joueur 1 qui a gagné !" << endl;
+        }
+        else if(score2>score1){
+            cout << "C'est le Joueur 2 qui a gagné !" << endl;
+        }
+        else{
+            cout << "Egalité entre les deux joueurs !" << endl;
+        }
+    }
+    else{
+        cout << "FINI ! Votre score final :" << score1 << endl;
+    }
 }
